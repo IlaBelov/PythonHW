df9f8c28d430b86dfa0152517c59f8bf7839ff12
Илья Белов <ilabelov94@gmail.com>
2025-12-01 23:33:30 +0300

    Lesson3

---
+++ lesson_03/lesson_3_task_3.py
@@ -0,0 +1,19 @@
+from address import Address
+from mailing import Mailing
+
+to_address = Address("123456", "Москва", "Ленина", "10", "25")
+from_address = Address("654321", "Санкт-Петербург", "Пушкина", "15", "7")
+
+mailing = Mailing(
+    to_address=to_address,
+    from_address=from_address,
+    cost=7550.50,
+    track="TRK123456789"
+)
+
+print(f"Отправление {mailing.track} из {mailing.from_address.index}, "
+      f"{mailing.from_address.city}, {mailing.from_address.street}, "
+      f"{mailing.from_address.house} - {mailing.from_address.apartment} в "
+      f"{mailing.to_address.index}, {mailing.to_address.city}, "
+      f"{mailing.to_address.street}, {mailing.to_address.house} - "
+      f"{mailing.to_address.apartment}. Стоимость {mailing.cost} рублей.")
