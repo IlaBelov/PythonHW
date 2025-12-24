5af77e449a3ecc92f8bcd7686b36fcbedf89fca2
Илья Белов <ilabelov94@gmail.com>
2025-12-24 03:21:34 +0300

    Change task 3

--- lesson_06/lesson06_task3.py
+++ lesson_06/lesson06_task3.py
@@ -3,7 +3,6 @@ from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 
 driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()
                                                 ))
@@ -12,10 +11,9 @@ waiter = WebDriverWait(driver, 40)
 driver.get("https://bonigarcia.dev/selenium-webdriver-java/loading-images.html"
            )
 
-waiter.until(
-    EC.text_to_be_present_in_element((By.CSS_SELECTOR, "#text"), "Done!")
-)
+waiter.until(lambda d: len(d.find_elements
+                           (By.CSS_SELECTOR, "#image-container img")) >= 4)
 
-print(driver.find_element(By.CSS_SELECTOR, "#award").get_attribute("src"))
+print(driver.find_element(By.ID, "award").get_attribute("src"))
 
 driver.quit()
